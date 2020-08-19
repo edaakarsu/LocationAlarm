36e41b83686e30512f0b839535103e9b8b272ce0
edakarsu <edakarsu9.12@gmail.cm>
2020-08-19 19:32:44 +0300

    firstcommit

---
+++ app/src/main/java/com/eu/locationalarm/MainSelectProfile.java
@@ -0,0 +1,14 @@
+package com.eu.locationalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainSelectProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_select_profile);
+    }
+}
